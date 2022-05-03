5352f766e3141116e1729e26e5daafd9f839ec93
Sir.Onnti <nicolasbaseggio1@gmail.com>
2022-05-03 14:03:34 -0300

    componente de lista de items

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-task';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+@Component({
+
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = TASKS[0]
+  faTimes = faTimes ;
+  constructor() { }
+
+  
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/mock-task.ts
@@ -0,0 +1,23 @@
+import { Task } from "./Task"
+export const TASKS:Task[] = [
+    {
+        id : 1, 
+        text : "terminar el primer moduilo de angular",
+        day : "agosto 512312 a las 141",
+        reminder : true , 
+    }
+    ,
+    {
+        id : 2, 
+        text : "alita" ,
+        day : "agosto  a las 141" ,
+        reminder : true , 
+    }
+    ,
+    {
+        id : 3, 
+        text : "caca" ,
+        day : "domingo a las 141" ,
+        reminder : false , 
+    }
+]
