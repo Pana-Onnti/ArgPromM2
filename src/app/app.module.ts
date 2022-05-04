402fdbfebcd7d37265a34a28b46d2730c51c00df
Sir.Onnti <nicolasbaseggio1@gmail.com>
2022-05-04 17:00:24 -0300

    termine loco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,16 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const appRoutes: Routes = [
+  {path: '' , component : TasksComponent},
+  {path: 'about' , component : AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -16,11 +25,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title:string = 'task-list';
-  constructor() { }
+  title:string = 'My Task List' ;
+  showAddTask:boolean=true;
+  subscription? : Subscription ;
+
+  
+  
+  constructor(
+    private router:Router, 
+    private uiService:UiService 
+
+  ) { 
+    this.subscription = this.uiService.onToggle()
+    .subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
   toggleAddTask(){
-    console.log("toogle");
+    this.uiService.toggleAddTask();
   }
+  hasRoute(route:string){
+    return this.router.url === route 
+  }
+
+
 }
+
